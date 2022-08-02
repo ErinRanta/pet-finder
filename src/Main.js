53f95c34e59841c638c52d3267561a7994294da8
Lucy Gelderloos <lucygel@gmail.com>
2022-08-02 10:34:41 -0700

    Add Pet to Main

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import SearchForm from './SearchForm'
+import Pet from './component/pet'
 
 class Main extends React.Component {
 
     render () {
         return (
+            <>
             <SearchForm />
+            <Pet />
+            </>
         )
     }
 }
