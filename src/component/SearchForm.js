2a67f76abaa26101ce66bc34b74886ef32cc51e9
Lucy Gelderloos <lucygel@gmail.com>
2022-08-03 10:21:58 -0700

    Query returns data from server; create Login

--- src/component/SearchForm.js
+++ src/component/SearchForm.js
@@ -22,57 +22,57 @@ class SearchForm extends React.Component {
         this.serverUrl = 'serverUrl'
     }
 
-    handleChange = (e) => {
+    // handleChange = (e) => {
     
-        // let {name, value} = e.target;
-        let name = e.target.name;
-        let value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-        this.setState({ [name]: value });
-    }
+    //     // let {name, value} = e.target;
+    //     let name = e.target.name;
+    //     let value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    //     this.setState({ [name]: value });
+    // }
     
-    handleSelectChange = (e) => {
-        let value = e.target.value;
-        value === 'zip' ? this.setState({searchByCity:true}) : this.setState({searchByCity:false});
-    }
+    // handleSelectChange = (e) => {
+    //     let value = e.target.value;
+    //     value === 'zip' ? this.setState({searchByCity:true}) : this.setState({searchByCity:false});
+    // }
     
-    handleSubmit = (e) => {
-        e.preventDefault();
+    // handleSubmit = (e) => {
+    //     e.preventDefault();
 
-        let location = this.state.searchByCity ? `cityName=${this.state.cityName}` : `zip=${this.state.zip}`;
-        // location is set to search by either zip code or cityName
-        let hasKids = this.state.hasKids ? `&hasKids=true` : '';
-        let hasCat = this.state.hasCat ? `&hasCat=true` : '';
-        let hasDog = this.state.hasDog ? `&hasDog=true` : '';
-        let hasAllergy = this.state.hasAllergy ? `&hasAllergy=true` : '';
+    //     let location = this.state.searchByCity ? `cityName=${this.state.cityName}` : `zip=${this.state.zip}`;
+    //     // location is set to search by either zip code or cityName
+    //     let hasKids = this.state.hasKids ? `&hasKids=true` : '';
+    //     let hasCat = this.state.hasCat ? `&hasCat=true` : '';
+    //     let hasDog = this.state.hasDog ? `&hasDog=true` : '';
+    //     let hasAllergy = this.state.hasAllergy ? `&hasAllergy=true` : '';
         
-        let searchUrl = `${this.serverUrl}/pets?${location}${hasKids}${hasCat}${hasDog}${hasAllergy}`;
+    //     let searchUrl = `${this.serverUrl}/pets?${location}${hasKids}${hasCat}${hasDog}${hasAllergy}`;
         
-        console.log(searchUrl);
+    //     console.log(searchUrl);
         
-        // axios.get(searchUrl)
-        // .then(response => {
-            //     this.setState({petResults:response});
-            // })
-            // .catch(err => {
-                //     console.log('error SearchForm handleSubmit',err);
-                //     this.setState({error:`Sorry, that search doesn't have any results! (${err.code}: ${err.message})`});
-                // })
-            }
+    //     // axios.get(searchUrl)
+    //     // .then(response => {
+    //         //     this.setState({petResults:response});
+    //         // })
+    //         // .catch(err => {
+    //             //     console.log('error SearchForm handleSubmit',err);
+    //             //     this.setState({error:`Sorry, that search doesn't have any results! (${err.code}: ${err.message})`});
+    //             // })
+    //         }
             
     render() {
         console.log('render this.state',this.state);
         return(
             <Form id="searchForm">
                 <Form.Group id="cityOrZip">
-                    <Form.Select name="searchByCity" onChange={this.handleSelectChange}>
+                    <Form.Select name="searchByCity" onChange={this.props.handleSelectChange}>
                         <option value="city">Search by zip code</option>
                         <option value="zip">Search by city</option>
                     </Form.Select>
                     <div className="searchBy">
                         {this.state.searchByCity ? 
-                            <Form.Control type="text" name="cityName" placeholder="City" onChange={this.handleChange} />
+                            <Form.Control type="text" name="cityName" placeholder="City" onChange={this.props.handleChange} />
                         : 
-                            <Form.Control type="text" name="zip" placeholder="Zip Code" onChange={this.handleChange} />
+                            <Form.Control type="text" name="zip" placeholder="Zip Code" onChange={this.props.handleChange} />
                         }
                     </div>
                 </Form.Group>
@@ -85,30 +85,30 @@ class SearchForm extends React.Component {
                             <Form.Check 
                                 name="hasKids"
                                 label="I need a pet who's good with kids"
-                                onChange={this.handleChange}
+                                onChange={this.props.handleChange}
                                 />
 
                             <Form.Check 
                                 name="hasCat"
                                 label="I need a pet who's good with other cats"
-                                onChange={this.handleChange}
+                                onChange={this.props.handleChange}
                                 />
 
                             <Form.Check 
                                 name="hasDog"
                                 label="I need a pet who's good with other dogs"
-                                onChange={this.handleChange}
+                                onChange={this.props.handleChange}
                                 />
 
                             <Form.Check 
                                 name="hasAllergy"
                                 label="I need a pet who's hypo-allergenic"
-                                onChange={this.handleChange}
+                                onChange={this.props.handleChange}
                                 />
                         </Dropdown.Menu>
                     </Dropdown>
                 </Form.Group>
-                <Button className="pink" type="submit" onClick={this.handleSubmit}>Search</Button>
+                <Button className="pink" type="submit" onClick={this.props.handleSubmit}>Search</Button>
             </Form>)}
 }
 
